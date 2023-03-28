be04ce89e21de81f9e2348323b24f8317d83714f
stratospher <44024636+stratospher@users.noreply.github.com>
2023-03-29 02:44:03 +0530

    huh, what is this? did you finish tried

--- addrman.py
+++ addrman.py
@@ -1,29 +1,53 @@
-import hashlib
+from hashlib import sha256
 
 ADDRMAN_NEW_BUCKETS_PER_SOURCE_GROUP = 64
 ADDRMAN_NEW_BUCKET_COUNT = 1024
 
-def get_group(ip):
+def get_group_ipv4(ip):
     ip_as_bytes = bytes(map(int, ip.split('.')))
     return bytes([1]) + ip_as_bytes[:2]
 
-def get_new_bucket(key, addr, src):
-    single_hash1 = hashlib.sha256(key + (3).to_bytes(1,'big') + get_group(addr) + (3).to_bytes(1,'big') + get_group(src)).digest()
-    #print("hash", single_hash1.hex()) # in c++ code: "4d56a13ae003f52efcd14ad513ac3a874916caf91f518612016d943199584c59"
-    double_hash1 = hashlib.sha256(single_hash1).digest()
-    #print("double", double_hash1.hex()) # in c++ code: "d2a744abb8cacd63d0466f0b4f7021d2662af06694c8a488726cfe5b9eaab89c"
-    hash1 =  int.from_bytes(double_hash1[:8], 'little') % ADDRMAN_NEW_BUCKETS_PER_SOURCE_GROUP
-    single_hash2 = hashlib.sha256(key + (3).to_bytes(1,'big') + get_group(addr) + hash1.to_bytes(8, 'little')).digest()
-    double_hash2 = hashlib.sha256(single_hash2).digest()
-    hash2 = int.from_bytes(double_hash2[:8], 'little')
-    return hash2 % ADDRMAN_NEW_BUCKET_COUNT
+def double_hash(bytes):
+    return sha256(sha256(bytes).digest()).digest()
 
-single_hash1 = hashlib.sha256((1).to_bytes(4, 'little')).digest()
-# print(single_hash1.hex())
-double_hash1 = hashlib.sha256(single_hash1).digest()
-# print(double_hash1.hex())
+def get_new_bucket(key, addr, src):
+    addr_group = get_group_ipv4(addr)
+    src_group = get_group_ipv4(src)
+    hash1 =  int.from_bytes(double_hash(key + bytes([len(addr_group)]) + addr_group
+                                            + bytes([len(src_group)]) + src_group)[:8], 'little')
+    hash1 = hash1 % ADDRMAN_NEW_BUCKETS_PER_SOURCE_GROUP #64
+    hash2 = int.from_bytes(double_hash(key + bytes([len(src_group)]) + src_group
+                                           + hash1.to_bytes(8, 'little'))[:8], 'little')
+    return hash2 % ADDRMAN_NEW_BUCKET_COUNT #1024
+
 key = bytes.fromhex("41f758f2e5cc078d3795b4fc0cb60c2d735fa92cc020572bdc982dd2d564d11b")
 addr = "250.1.2.1"
 src = "250.1.2.1"
 bucket = get_new_bucket(key, addr, src)
 print("bucket is", bucket) # 786
+
+ADDRMAN_TRIED_BUCKETS_PER_GROUP = 8
+ADDRMAN_TRIED_BUCKET_COUNT = 256
+def get_key(ip, port):
+    ip_as_bytes = bytes(map(int, ip.split('.')))
+    print(ip_as_bytes.hex())
+    print((port // 0x100).to_bytes(1, 'little').hex())
+    print((port & 0x0FF).to_bytes(1, 'little').hex())
+    return ip_as_bytes + (port // 0x100).to_bytes(1, 'little') + (port & 0x0FF).to_bytes(1, 'little')
+
+def get_tried_bucket(key, addr, port):
+    addr_group = get_group_ipv4(addr)
+    addr_id = get_key(addr, port) #00000000000000000000ffff fa010101208d
+    print(addr_id.hex())
+    hash1 = int.from_bytes(double_hash(key + bytes([len(addr_id)]) + addr_id), 'little')
+    hash1 = hash1 % ADDRMAN_TRIED_BUCKETS_PER_GROUP
+    hash2 = int.from_bytes(double_hash(key + bytes([len(addr_group)]) + addr_group
+                                           + hash1.to_bytes(8, 'little'))[:8], 'little')
+    return hash2 % ADDRMAN_TRIED_BUCKET_COUNT
+
+key = bytes.fromhex("1bd164d5d22d98dc2b5720c02ca95f732d0cb60cfcb495378d07cce5f258f741")
+addr = "250.1.1.1"
+port = 8333
+src = "250.1.1.1"
+bucket = get_tried_bucket(key, addr, port)
+print("bucket is", bucket)
